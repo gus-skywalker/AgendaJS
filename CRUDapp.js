d7f26f45cf68cf6b026696955aa001021cd1e154
gus-skywalker <gustavoxaviercontato@gmail.com>
2021-02-03 10:35:08 -0300

    Novo repositorio criado para teste CRUD em Javascript

---
+++ CRUDapp.js
@@ -0,0 +1,143 @@
+let CRUDapp = new function() {
+
+  // Variável "Elemento Global" com Lista de Contatos dentro do APP
+  this.contatos = [
+    {
+      nome: "rinoceronte",
+      telefone: "+1 38383838",
+      email: "callme@teste.us"
+    },
+    {
+      nome: "rinoceronte2",
+      telefone: "+1 222222222",
+      email: "callme@teste.us"
+    },
+    {
+      nome: "rinoceronte3",
+      telefone: "+1 3333333",
+      email: "callme@teste.us"
+    }
+  ];
+
+  this.el = document.getElementById('contatos'); // Variável Elemento Global
+
+  this.Count = function(data) {
+      let el = document.getElementById('counter'); // Variável Elemento de Escopo Local
+      let name = 'Contatos'; // Variável dentro da função a ser renomeada caso haja contatos
+
+      if (data) {
+        if (data > 1) {
+            name = 'contatos na sua agenda';
+        }
+          el.innerHTML = data + ' ' + name ;
+        } else {
+          el.innerHTML = 'Sem ' + name;
+      }
+};
+
+  this.FetchAll = function() {
+      let data = ''; // Inicializar Variável de Escopo Local
+
+      if (this.contatos.length > 0) { // Recupera Variável de Contatos Global e faz um loop nos Objetos da Matriz
+        for (o = 0; o < this.contatos.length; o++) {
+          data += '<tr class="contato">';
+          data += '<td class="contato_nome">' + this.contatos[o].nome + '</td>';
+          data += '<td class="contato_tel">' + this.contatos[o].telefone + '</td>';
+          data += '<td class="contato_email">' + this.contatos[o].email + '</td>';
+          data += '<td><button onclick="CRUDapp.Edit(' + o + ')">Edit</button></td>'; // Insere botão Edição na própria lista de contatos
+          data += '<td><button onclick="CRUDapp.Delete(' + o + ')">Delete</button></td>'; //Insere botão Delete na própria lista de contatos
+          data += '</tr>';
+        }
+
+        // for (const prop in data)
+      }
+        this.Count(this.contatos.length); // Executa o contador
+        return this.el.innerHTML = data; // Retorna o input dos dados do laço For() no Elemento Global 'el'
+    };
+
+    this.Add = function () {
+        el1 = document.getElementById('add-nome');
+        el2 = document.getElementById('add-telefone');
+        el3 = document.getElementById('add-email');
+        // Pega as variáveis do campo de input do form
+        let nome = el1.value;
+        let telefone = el2.value;
+        let email = el3.value;
+
+        if (nome && telefone && email != null) {
+          // Acrescenta os novos valores na lista global contatos
+          this.contatos.push({nome: nome, telefone: telefone, email: email});
+          //TESTE DA EDICAO: OK! this.contatos.splice (0, 1, {nome: "eu", telefone: "2", email: "3"});
+          // Reseta os campos do input do form
+          el1.value = '';
+          el2.value = '';
+          el3.value = '';
+          // Mostra a nova lista
+          console.log('Inserçao com sucesso'); // Confirma no console --> *** Retirar depois ****
+          this.FetchAll();
+        } else {
+          alert("Todos os campos devem ser preenchidos");
+        }
+      };
+
+      this.Edit = function (item) {
+        let el1 = document.getElementById('edit-name');
+        let el2 = document.getElementById('edit-telefone');
+        let el3 = document.getElementById('edit-email');
+        // Preenche os campos com os valores
+        el1.value = this.contatos[item].nome;
+        el2.value = this.contatos[item].telefone;
+        el3.value = this.contatos[item].email;
+
+        console.log('Item selecionado:' + item); // Retirar
+
+        // Mostra o campo de alteração
+        document.getElementById('spoiler').style.display = 'block';
+        self = this; // Self vs. this Para retornar objeto da janela atual "window.self"
+
+        document.getElementById('saveEdit').onsubmit = function() {
+          // Recebe valor dos novos itens
+          let novo_nome = el1.value;
+          let novo_telefone = el2.value;
+          let novo_email = el3.value;
+
+          let novo_contato =  {
+            nome: el1.value,
+            telefone: el2.value,
+            email: el3.value
+          };
+
+          console.log(novo_contato); // Retirar
+
+          if (Array.isArray(self.contatos[item])) {
+            console.log('Contatos é uma Array');
+          } else {
+            console.log('Contatos NÃO é uma Array !!');
+          };
+          // if (novo_nome && novo_telefone && novo_email != null && Array.isArray(self.contatos)) {
+          //   // Edita os novos valores
+          //   this.contatos.splice(item, 1, novo_contato);
+          //   // Mostra a nova lista
+          //   self.FetchAll();
+          //   // Esconde o campo
+          //   CloseInput();
+          // } else {
+          //   alert("Todos os campos devem ser preenchidos");
+          // }
+        }
+      };
+
+      this.Delete = function (item) {
+        // Deleta a linha escolhida
+        this.contatos.splice(item, 1);
+        // Mostra a nova lista
+        this.FetchAll();
+      };
+
+}
+
+function CloseInput() {
+  document.getElementById('spoiler').style.display = 'none';
+}
+
+CRUDapp.FetchAll();
